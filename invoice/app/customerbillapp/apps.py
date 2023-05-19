67ca51a2427b94bf13d95f50ebb787ca9a10d5fd
krishnaappsphere <krisna.sisodiya@appsphere.in>
2023-05-19 18:50:41 +0530

    CBM-1 : Project setup

---
+++ invoice/app/customerbillapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CustomerbillappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app.customerbillapp'
--- invoice/app/settings.py
+++ invoice/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "django_celery_beat",
+    'app.customerbillapp',
 ]
 
 MIDDLEWARE = [
@@ -73,7 +74,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
